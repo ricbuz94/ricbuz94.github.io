516b26edafb7cff16e345868209fd3e2c51d0714
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2021-05-30 14:22:55 +0200

    FIX routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,9 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  //   Redirect,
-} from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { Home, DrinkToolPolicy, PageNotFound } from "./pages";
 
 const App = () => (
   <Router>
     <Switch>
-      {/* <Route exact path="/" component={Home}>
-        <Redirect to="/" />
-      </Route> */}
       <Route path="/" children={<Home />} />
       <Route path="/drinktool-policy" children={<DrinkToolPolicy />} />
       <Route path="/*" children={<PageNotFound />} />
