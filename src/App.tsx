effc0908abcd576daf209a705789be675d6f407f
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2021-05-30 13:58:37 +0200

    FIX routing on gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,17 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect,
+  //   Redirect,
 } from "react-router-dom";
 import { Home, DrinkToolPolicy, PageNotFound } from "./pages";
 
 const App = () => (
   <Router>
     <Switch>
-      <Route exact path="/" component={Home}>
-        <Redirect to="/home" />
-      </Route>
-      <Route path="/home" children={<Home />} />
+      {/* <Route exact path="/" component={Home}>
+        <Redirect to="/" />
+      </Route> */}
+      <Route path="/" children={<Home />} />
       <Route path="/drinktool-policy" children={<DrinkToolPolicy />} />
       <Route path="/*" children={<PageNotFound />} />
     </Switch>
