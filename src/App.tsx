30922d65f28eaa603a78dd606dcc9ab4de42ff1e
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2021-05-30 14:30:52 +0200

    FIX routing

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Home, DrinkToolPolicy, PageNotFound } from "./pages";
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/" children={<Home />} />
+      <Route exact path="/" children={<Home />} />
       <Route path="/drinktool-policy" children={<DrinkToolPolicy />} />
       <Route path="/*" children={<PageNotFound />} />
     </Switch>
