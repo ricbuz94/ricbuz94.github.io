2ec6229e23aee5739232dfecaa7037f65e03fe21
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2021-12-11 00:42:58 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,35 @@
-const timestamp = 1639177560446;
+const timestamp = 1639179776274;
 const build = [
-  "/_app/start-afef0a5b.js",
-  "/_app/assets/start-61d1577b.css",
-  "/_app/pages/__layout.svelte-f962bdb8.js",
-  "/_app/assets/pages/__layout.svelte-ab021dd4.css",
-  "/_app/pages/__error.svelte-48e92e36.js",
-  "/_app/pages/index.svelte-e71f4f6e.js",
-  "/_app/assets/pages/index.svelte-4f3c9d4b.css",
-  "/_app/pages/drinktool-policy.svelte-7224cf2e.js",
-  "/_app/assets/pages/drinktool-policy.svelte-f584f4d3.css",
-  "/_app/pages/_fallthough_/_fallthough_.svelte-494930e3.js",
-  "/_app/assets/pages/_fallthough_.svelte-2af5e13b.css",
-  "/_app/pages/_fallthough_.svelte-9cfb5b42.js",
-  "/_app/chunks/vendor-8b76c6b9.js",
-  "/_app/chunks/paths-c3a241ee.js",
-  "/_app/chunks/navigation-d8ceaada.js"
+  "/me/app/start-b9473122.js",
+  "/me/app/assets/start-61d1577b.css",
+  "/me/app/pages/__layout.svelte-f962bdb8.js",
+  "/me/app/assets/pages/__layout.svelte-ab021dd4.css",
+  "/me/app/pages/__error.svelte-48e92e36.js",
+  "/me/app/pages/index.svelte-e71f4f6e.js",
+  "/me/app/assets/pages/index.svelte-4f3c9d4b.css",
+  "/me/app/pages/drinktool-policy.svelte-7224cf2e.js",
+  "/me/app/assets/pages/drinktool-policy.svelte-f584f4d3.css",
+  "/me/app/pages/_fallthough_/_fallthough_.svelte-494930e3.js",
+  "/me/app/assets/pages/_fallthough_.svelte-2af5e13b.css",
+  "/me/app/pages/_fallthough_.svelte-9cfb5b42.js",
+  "/me/app/chunks/vendor-8b76c6b9.js",
+  "/me/app/chunks/paths-c3a241ee.js",
+  "/me/app/chunks/navigation-d8ceaada.js"
 ];
 const files = [
-  "/.nojekyll",
-  "/drink-tool.ico",
-  "/favicon.ico",
-  "/feather-sprite.svg",
-  "/fonts/RobotoMono-Variable.ttf",
-  "/fonts/Ubuntu/UFL.txt",
-  "/fonts/Ubuntu/Ubuntu-Bold.ttf",
-  "/fonts/Ubuntu/Ubuntu-Light.ttf",
-  "/fonts/Ubuntu/Ubuntu-Medium.ttf",
-  "/fonts/Ubuntu/Ubuntu-Regular.ttf",
-  "/logo-dark.png",
-  "/logo-light.png",
-  "/manifest.json",
-  "/robots.txt"
+  "/me/drink-tool.ico",
+  "/me/favicon.ico",
+  "/me/feather-sprite.svg",
+  "/me/fonts/RobotoMono-Variable.ttf",
+  "/me/fonts/Ubuntu/UFL.txt",
+  "/me/fonts/Ubuntu/Ubuntu-Bold.ttf",
+  "/me/fonts/Ubuntu/Ubuntu-Light.ttf",
+  "/me/fonts/Ubuntu/Ubuntu-Medium.ttf",
+  "/me/fonts/Ubuntu/Ubuntu-Regular.ttf",
+  "/me/logo-dark.png",
+  "/me/logo-light.png",
+  "/me/manifest.json",
+  "/me/robots.txt"
 ];
 const worker = self;
 const FILES = `cache${timestamp}`;
