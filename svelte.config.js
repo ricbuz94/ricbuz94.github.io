b16b6dec10af841ace0f0004cc62dd2a468dd3b4
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2021-12-10 21:42:49 +0100

    FIX deploy configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import preprocess from 'svelte-preprocess';
 import static_adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
@@ -8,8 +10,8 @@ const config = {
 		ssr: false,
 		adapter: static_adapter(),
 		paths: {
-			base: '/app'
-		}
+			base: dev ? '' : '/app'
+		},
 	}
 };
 
