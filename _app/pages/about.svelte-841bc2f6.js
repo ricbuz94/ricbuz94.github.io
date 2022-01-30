7eba6e2c43ddc31ea7ea5a86c94f607d6efb91b8
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2022-01-30 19:59:25 +0100

    Updates

---
+++ _app/pages/about.svelte-841bc2f6.js
@@ -0,0 +1,30 @@
+import{S as X,i as Y,s as tt,k as z,e as g,j as h,V as et,d,n as B,c as $,a as E,m as p,Z as K,b as m,f as q,o as v,G as c,x as w,u as _,v as b,t as T,g as V,H as Q}from"../chunks/vendor-8c17374b.js";import{D as L}from"../chunks/Divider-e07c07ff.js";import{A as W,a as G}from"../chunks/Article-d0dc9fea.js";import"../chunks/paths-28a87002.js";function nt(R){let t,e,n,f,a,r,l,y;return n=new G({props:{title:`State
+				Technical Technological Institute`,href:"https://www.ittsrimini.edu.it"}}),a=new G({props:{title:"Engineering and Computer Science",href:"https://corsi.unibo.it/laurea/IngegneriaScienzeInformatiche"}}),l=new G({props:{title:"Beacharound S.r.l.",href:"https://www.beacharound.com"}}),{c(){t=g("p"),e=T(`Born in Cattolica (RN) on 12/13/1994, in 2013 I graduated from the
+					then Technical Institute for Surveyors: Odone Belluzzi of Rimini
+					(today `),h(n.$$.fragment),f=T(`). I studied at the University of Cesena, Alma Mater Studiorum, in
+					the course of `),h(a.$$.fragment),r=T(`. I currently work as a CTO and full-stack web developer for an
+					innovative startup that deals with beach management and online booking
+					of establishments:
+					`),h(l.$$.fragment),this.h()},l(i){t=$(i,"P",{class:!0});var o=E(t);e=V(o,`Born in Cattolica (RN) on 12/13/1994, in 2013 I graduated from the
+					then Technical Institute for Surveyors: Odone Belluzzi of Rimini
+					(today `),p(n.$$.fragment,o),f=V(o,`). I studied at the University of Cesena, Alma Mater Studiorum, in
+					the course of `),p(a.$$.fragment,o),r=V(o,`. I currently work as a CTO and full-stack web developer for an
+					innovative startup that deals with beach management and online booking
+					of establishments:
+					`),p(l.$$.fragment,o),o.forEach(d),this.h()},h(){m(t,"class","svelte-1xjunwo")},m(i,o){q(i,t,o),c(t,e),v(n,t,null),c(t,f),v(a,t,null),c(t,r),v(l,t,null),y=!0},p:Q,i(i){y||(w(n.$$.fragment,i),w(a.$$.fragment,i),w(l.$$.fragment,i),y=!0)},o(i){_(n.$$.fragment,i),_(a.$$.fragment,i),_(l.$$.fragment,i),y=!1},d(i){i&&d(t),b(n),b(a),b(l)}}}function st(R){let t,e,n,f,a;return n=new G({props:{title:"Beacharound S.r.l.",href:"https://business.beacharound.com/gestionale-spiaggia"}}),{c(){t=g("p"),e=T(`As mentioned above, i currently work for an innovative startup that
+					sells a management system for bath establishments and online bookings:
+					`),h(n.$$.fragment),f=T(` During my winter working days I take care of testing new management
+					software functions, reporting bugs to developers (sometimes developing
+					some functions myself) and providing technical assistance to business customers.
+					During the summer, the company concentrates its resources on the management
+					of online bookings (business side and customer side). In that case, my
+					work also aligns with these needs and I spend my time providing technical
+					assistance by reporting bugs to the developers.`),this.h()},l(r){t=$(r,"P",{class:!0});var l=E(t);e=V(l,`As mentioned above, i currently work for an innovative startup that
+					sells a management system for bath establishments and online bookings:
+					`),p(n.$$.fragment,l),f=V(l,` During my winter working days I take care of testing new management
+					software functions, reporting bugs to developers (sometimes developing
+					some functions myself) and providing technical assistance to business customers.
+					During the summer, the company concentrates its resources on the management
+					of online bookings (business side and customer side). In that case, my
+					work also aligns with these needs and I spend my time providing technical
+					assistance by reporting bugs to the developers.`),l.forEach(d),this.h()},h(){m(t,"class","svelte-1xjunwo")},m(r,l){q(r,t,l),c(t,e),v(n,t,null),c(t,f),a=!0},p:Q,i(r){a||(w(n.$$.fragment,r),a=!0)},o(r){_(n.$$.fragment,r),a=!1},d(r){r&&d(t),b(n)}}}function at(R){let t,e,n,f,a,r,l,y,i,o,N,x,P,k,C,I,U,A,Z,H,D,M;return n=new L({}),o=new W({props:{title:"About me",$$slots:{default:[nt]},$$scope:{ctx:R}}}),x=new L({}),I=new W({props:{title:"About my work",$$slots:{default:[st]},$$scope:{ctx:R}}}),D=new L({}),{c(){t=z(),e=g("div"),h(n.$$.fragment),f=z(),a=g("div"),r=g("img"),y=z(),i=g("div"),h(o.$$.fragment),N=z(),h(x.$$.fragment),P=z(),k=g("div"),C=g("div"),h(I.$$.fragment),U=z(),A=g("img"),H=z(),h(D.$$.fragment),this.h()},l(s){et('[data-svelte="svelte-1nf9xoo"]',document.head).forEach(d),t=B(s),e=$(s,"DIV",{class:!0});var u=E(e);p(n.$$.fragment,u),f=B(u),a=$(u,"DIV",{class:!0});var S=E(a);r=$(S,"IMG",{src:!0,alt:!0,class:!0}),y=B(S),i=$(S,"DIV",{class:!0});var F=E(i);p(o.$$.fragment,F),F.forEach(d),S.forEach(d),N=B(u),p(x.$$.fragment,u),P=B(u),k=$(u,"DIV",{class:!0});var O=E(k);C=$(O,"DIV",{class:!0});var J=E(C);p(I.$$.fragment,J),J.forEach(d),U=B(O),A=$(O,"IMG",{src:!0,alt:!0,class:!0}),O.forEach(d),H=B(u),p(D.$$.fragment,u),u.forEach(d),this.h()},h(){document.title="About \xB7 RiccardoBuzzolo",K(r.src,l="/images/me.jpg")||m(r,"src",l),m(r,"alt","Riccardo Buzzolo"),m(r,"class","svelte-1xjunwo"),m(i,"class","card-wide svelte-1xjunwo"),m(a,"class","content svelte-1xjunwo"),m(C,"class","card-wide svelte-1xjunwo"),K(A.src,Z="/images/beacharound_business_info.png")||m(A,"src",Z),m(A,"alt","Beacharound"),m(A,"class","svelte-1xjunwo"),m(k,"class","content dense svelte-1xjunwo"),m(e,"class","container svelte-1xjunwo")},m(s,j){q(s,t,j),q(s,e,j),v(n,e,null),c(e,f),c(e,a),c(a,r),c(a,y),c(a,i),v(o,i,null),c(e,N),v(x,e,null),c(e,P),c(e,k),c(k,C),v(I,C,null),c(k,U),c(k,A),c(e,H),v(D,e,null),M=!0},p(s,[j]){const u={};j&1&&(u.$$scope={dirty:j,ctx:s}),o.$set(u);const S={};j&1&&(S.$$scope={dirty:j,ctx:s}),I.$set(S)},i(s){M||(w(n.$$.fragment,s),w(o.$$.fragment,s),w(x.$$.fragment,s),w(I.$$.fragment,s),w(D.$$.fragment,s),M=!0)},o(s){_(n.$$.fragment,s),_(o.$$.fragment,s),_(x.$$.fragment,s),_(I.$$.fragment,s),_(D.$$.fragment,s),M=!1},d(s){s&&d(t),s&&d(e),b(n),b(o),b(x),b(I),b(D)}}}class ct extends X{constructor(t){super();Y(this,t,null,at,tt,{})}}export{ct as default};
--- _app/pages/drinktool/index.svelte-49dffede.js
+++
@@ -1,1 +0,0 @@
-import{S as t,i as e,s as n}from"../../chunks/vendor-6657840f.js";class a extends t{constructor(s){super();e(this,s,null,null,n,{})}}export{a as default};
