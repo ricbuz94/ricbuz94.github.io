89e1884d874ad8baf1a7fd9dc2622e27aa18c57d
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2023-05-13 23:15:55 +0200

    Updates

--- internal/immutable/chunks/0-b08483f8.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_layout-5305f73c.js";import{default as t}from"../components/pages/_layout.svelte-cd475c29.js";export{t as component,r as universal};
--- internal/immutable/chunks/9-fcd8edd2.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/works/_page.svelte-4b6510d4.js";export{t as component};
