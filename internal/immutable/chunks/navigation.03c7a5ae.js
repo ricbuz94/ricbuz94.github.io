b32951e731c68e244c0f1a2be89144391b51355c
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2023-11-25 15:21:34 +0100

    Updates

---
+++ internal/immutable/chunks/navigation.03c7a5ae.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.ab6a6890.js";const e=o("goto"),a=o("before_navigate");export{a as b,e as g};
--- internal/immutable/chunks/navigation.1c5ee467.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.c42581aa.js";const e=o("goto"),a=o("before_navigate");export{a as b,e as g};
