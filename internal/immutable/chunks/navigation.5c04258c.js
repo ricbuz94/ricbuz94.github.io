14d4c6c82ffdcee5f3a7ab10069a04020915bf0a
Riccardo Buzzolo <riccardo.buzzolo@gmail.com>
2024-11-07 12:32:19 +0100

    Updates

---
+++ internal/immutable/chunks/navigation.5c04258c.js
@@ -0,0 +1,1 @@
+import{h as o}from"./singletons.9a16243b.js";const e=o("goto"),a=o("before_navigate");export{a as b,e as g};
--- internal/immutable/chunks/navigation.a5786317.js
+++
@@ -1,1 +0,0 @@
-import{h as o}from"./singletons.a7394075.js";const e=o("goto"),a=o("before_navigate");export{a as b,e as g};
